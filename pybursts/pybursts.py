a28b2b04ca165e2965c0d29868d7fc5075ef85a9
romain.fontugne <romain.fontugne@gmail.com>
2016-07-07 17:44:26 +0900

    fixed cost function when g_hat is given

--- pybursts/pybursts.py
+++ pybursts/pybursts.py
@@ -24,13 +24,14 @@ def kleinberg(offsets, s=2, gamma=1, g_hat=None):
 		raise ValueError("Input cannot contain events with zero time between!")
 
         T = np.sum(gaps)
-        n = np.size(gaps)
         if g_hat is None:
             g_hat = T / n
+	    gamma_log_n = gamma * math.log(n)
+            n = np.size(gaps)
+        else :
+            gamma_log_n = gamma
 
 	k = int(math.ceil(float(1 + math.log(T, s) + math.log(1 / np.amin(gaps), s))))
-
-	gamma_log_n = gamma * math.log(n)
 
 	def tau(i, j):
 		if i >= j:
